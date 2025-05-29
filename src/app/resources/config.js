36568fd36acf62222fa258926ffd60eb64a6384a
Craig Putzstuck <putzstuck@gmail.com>
2025-05-29 16:34:11 -0400

    avatar and icon update

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -1,5 +1,5 @@
 // IMPORTANT: Replace with your own domain address - it's used for SEO in meta tags and schema
-const baseURL = "https://craigliam.com";
+const baseURL = "https://www.craigliam.com";
 
 const routes = {
   "/": true,
